fb166ca244b4d4a16b0bce4c62df34489c5cf23e
nelsonchung <chihchun.chung@gmail.com>
2016-06-02 11:38:20 +0800

    自動於星期一到五下午四點三十五分

--- Daily.sh
+++ Daily.sh
@@ -1,6 +1,21 @@
 
 STOCK_ID="2498"
-cp ../TaiwanStockMonitor/${STOCK_ID}.csv ./
-git add .
-git commit -am "Update ${STOCK_ID} information"
-git push
+HOUR_RUN_STOCK=16
+MINUTE_RUN_STOCK=35
+
+while [ 1 ]
+do
+    HOUR=`date +"%H"`
+    MINUTE=`date +"%M"`
+    DAY_OF_WEEK=`date +"%u"`
+    if [ "$DAY_OF_WEEK" == "1" ] || [ "$DAY_OF_WEEK" == "2" ] || [ "$DAY_OF_WEEK" == "3" ] || [ "$DAY_OF_WEEK" == "4" ] || [ "$DAY_OF_WEEK" == "5" ] ; then
+    echo "星期${DAY_OF_WEEK}, ${HOUR}點${MINUTE}分"
+        if [ "$HOUR" == "$HOUR_RUN_STOCK" ] && [ "$MINUTE" == "$MINUTE_RUN_STOCK" ]; then
+            cp ../TaiwanStockMonitor/${STOCK_ID}.csv ./
+            git add .
+            git commit -am "Update ${STOCK_ID} information"
+            git push
+        fi
+    fi
+    sleep 60
+done
