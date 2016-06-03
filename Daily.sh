da9c31cad8cb599d7bdbed9e9c67039d62ee1e2c
nelsonchung <chihchun.chung@gmail.com>
2016-06-03 23:06:40 +0800

    Fine tune Daily.sh to change file name from csv to txt.

--- Daily.sh
+++ Daily.sh
@@ -9,9 +9,9 @@ do
     MINUTE=`date +"%M"`
     DAY_OF_WEEK=`date +"%u"`
     if [ "$DAY_OF_WEEK" == "1" ] || [ "$DAY_OF_WEEK" == "2" ] || [ "$DAY_OF_WEEK" == "3" ] || [ "$DAY_OF_WEEK" == "4" ] || [ "$DAY_OF_WEEK" == "5" ] ; then
-    echo "星期${DAY_OF_WEEK}, ${HOUR}點${MINUTE}分"
+    echo "Monitor ${STOCK_ID} at 星期${DAY_OF_WEEK}, ${HOUR}點${MINUTE}分"
         if [ "$HOUR" == "$HOUR_RUN_STOCK" ] && [ "$MINUTE" == "$MINUTE_RUN_STOCK" ]; then
-            cp ../TaiwanStockMonitor/${STOCK_ID}.csv ./
+            cp ../TaiwanStockMonitor/${STOCK_ID}.txt ./
             git add .
             git commit -am "Update ${STOCK_ID} information"
             git push
